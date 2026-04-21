dcd88294969e8f1fee087d7f330221e3adea0335
250350 <piechotapoczta@wp.pl>
2026-04-21 13:47:01 +0200

    Added @ResponseBody to ping()

--- src/main/java/pl/comp/generatorfaktur/controller/HomeController.java
+++ src/main/java/pl/comp/generatorfaktur/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import pl.comp.generatorfaktur.entities.InvoiceItem;
@@ -41,6 +42,7 @@ public class HomeController {
     }
 
     @GetMapping("/ping")
+    @ResponseBody
     public String ping() {
         return "ok";
     }
